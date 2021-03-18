43bb8323fc6822581d0a67f5f056d2c93fd997bb
张昆 <zhangkun@lhcis.com>
2021-03-18 21:15:05 +0800

    support date str type of *天前

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='SomeTools',
-      version='0.0.3',
+      version='0.0.4',
       description="Some python tools",
       author="zhangkun",
       author_email="zk.kyle@foxmail.com",

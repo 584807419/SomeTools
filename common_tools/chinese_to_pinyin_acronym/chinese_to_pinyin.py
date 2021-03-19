782bdaffebd7060f6025be9a3b13f5e9daffe06a
张昆 <zhangkun@lhcis.com>
2021-03-19 09:59:00 +0800

    Perfect readme

--- common_tools/chinese_to_pinyin_acronym/chinese_to_pinyin.py
+++ common_tools/chinese_to_pinyin_acronym/chinese_to_pinyin.py
@@ -3,62 +3,8 @@ class GeneralChineseToPinyin:
     def __init__(self, *args, **kwargs):
         super(GeneralChineseToPinyin, self).__init__(*args, **kwargs)
 
-    def single_get_first(self, unicode1):
-        str1 = unicode1.encode('gbk')
-        try:
-            ord(str1)
-            return str1.decode('gbk')
-        except:
-            asc = str1[0] * 256 + str1[1] - 65536
-            if asc >= -20319 and asc <= -20284:
-                return 'a'
-            if asc >= -20283 and asc <= -19776:
-                return 'b'
-            if asc >= -19775 and asc <= -19219:
-                return 'c'
-            if asc >= -19218 and asc <= -18711:
-                return 'd'
-            if asc >= -18710 and asc <= -18527:
-                return 'e'
-            if asc >= -18526 and asc <= -18240:
-                return 'f'
-            if asc >= -18239 and asc <= -17923:
-                return 'g'
-            if asc >= -17922 and asc <= -17418:
-                return 'h'
-            if asc >= -17417 and asc <= -16475:
-                return 'j'
-            if asc >= -16474 and asc <= -16213:
-                return 'k'
-            if asc >= -16212 and asc <= -15641:
-                return 'l'
-            if asc >= -15640 and asc <= -15166:
-                return 'm'
-            if asc >= -15165 and asc <= -14923:
-                return 'n'
-            if asc >= -14922 and asc <= -14915:
-                return 'o'
-            if asc >= -14914 and asc <= -14631:
-                return 'p'
-            if asc >= -14630 and asc <= -14150:
-                return 'q'
-            if asc >= -14149 and asc <= -14091:
-                return 'r'
-            if asc >= -14090 and asc <= -13119:
-                return 's'
-            if asc >= -13118 and asc <= -12839:
-                return 't'
-            if asc >= -12838 and asc <= -12557:
-                return 'w'
-            if asc >= -12556 and asc <= -11848:
-                return 'x'
-            if asc >= -11847 and asc <= -11056:
-                return 'y'
-            if asc >= -11055 and asc <= -10247:
-                return 'z'
-            return ''
-
-    def get_pinyin(self, chinese_string: str) -> str:
+    @staticmethod
+    def get_pinyin(chinese_string: str) -> str:
         """
         输入中文字符串返回拼音的首字母字符串
         :param chinese_string: str
@@ -67,8 +13,60 @@ class GeneralChineseToPinyin:
         if chinese_string:
             lst = list(chinese_string)
             charLst = []
-            for l in lst:
-                charLst.append(self.single_get_first(l))
+            for unicode1 in lst:
+                str1 = unicode1.encode('gbk')
+                try:
+                    ord(str1)
+                    charLst.append(str1.decode('gbk'))
+                except:
+                    asc = str1[0] * 256 + str1[1] - 65536
+                    if asc >= -20319 and asc <= -20284:
+                        charLst.append('a')
+                    if asc >= -20283 and asc <= -19776:
+                        charLst.append('b')
+                    if asc >= -19775 and asc <= -19219:
+                        charLst.append('c')
+                    if asc >= -19218 and asc <= -18711:
+                        charLst.append('d')
+                    if asc >= -18710 and asc <= -18527:
+                        charLst.append('e')
+                    if asc >= -18526 and asc <= -18240:
+                        charLst.append('f')
+                    if asc >= -18239 and asc <= -17923:
+                        charLst.append('g')
+                    if asc >= -17922 and asc <= -17418:
+                        charLst.append('h')
+                    if asc >= -17417 and asc <= -16475:
+                        charLst.append('j')
+                    if asc >= -16474 and asc <= -16213:
+                        charLst.append('k')
+                    if asc >= -16212 and asc <= -15641:
+                        charLst.append('l')
+                    if asc >= -15640 and asc <= -15166:
+                        charLst.append('m')
+                    if asc >= -15165 and asc <= -14923:
+                        charLst.append('n')
+                    if asc >= -14922 and asc <= -14915:
+                        charLst.append('o')
+                    if asc >= -14914 and asc <= -14631:
+                        charLst.append('p')
+                    if asc >= -14630 and asc <= -14150:
+                        charLst.append('q')
+                    if asc >= -14149 and asc <= -14091:
+                        charLst.append('r')
+                    if asc >= -14090 and asc <= -13119:
+                        charLst.append('s')
+                    if asc >= -13118 and asc <= -12839:
+                        charLst.append('t')
+                    if asc >= -12838 and asc <= -12557:
+                        charLst.append('w')
+                    if asc >= -12556 and asc <= -11848:
+                        charLst.append('x')
+                    if asc >= -11847 and asc <= -11056:
+                        charLst.append('y')
+                    if asc >= -11055 and asc <= -10247:
+                        charLst.append('z')
+                    charLst.append('')
             return ''.join(charLst)
         return ''
 
--- main.py
+++ main.py
@@ -8,4 +8,23 @@ class Demo(Common_tools):
 
 if __name__ == '__main__':
     demo_ins = Demo()
+
+    # 将输入的任何类型的日期字符串类型转化为datetime.datetime类型的日期对象(Converts any type of date string type entered to a date object of type datetime.datetime)
     demo_ins.logger.info(f"{demo_ins.str_to_obj('2012-12-12 12:12:12')}{type(demo_ins.str_to_obj('2012-12-12 12:12:12'))}")
+
+    # 移除一个字符串中的回车换行空格制表符等内容(Remove carriage return, newline space tabs, etc., from a string)
+    temp_str = 'abc abc \n abc \r'
+    demo_ins.logger.info(f"before clean string{temp_str}")
+    temp_str = Demo.clean_string('abc abc \n abc \r')
+    demo_ins.logger.info(f"after clean string{temp_str}")
+
+    # 将中文字符转为其拼音的首字母(Convert Chinese characters to the first letter of their pinyin)
+    demo_ins.logger.info(f"{Demo.get_pinyin('中国外汇交易中心')}")
+
+    # 更加方便的日志记录工具(More convenient logging tool)
+    demo_ins.logger.info(f"{Demo.get_pinyin('中国外汇交易中心')}")
+    demo_ins.logger.debug(f"{Demo.get_pinyin('中国外汇交易中心')}")
+    demo_ins.logger.warning(f"{Demo.get_pinyin('中国外汇交易中心')}")
+    demo_ins.logger.error(f"{Demo.get_pinyin('中国外汇交易中心')}")
+    demo_ins.logger.exception(f"{Demo.get_pinyin('中国外汇交易中心')}")
+

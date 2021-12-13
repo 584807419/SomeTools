6e23769352ff3fec43eacfbb66fae9f65fe33f23
张昆 <zhangkun@lhcis.com>
2021-12-13 11:24:16 +0800

    [OPTIMIZE]Optimize
    1. use get_running_loop replace get_event_loop,avoid error:"https://github.com/libuv/libuv/issues/2387"

--- sometools/async_tools/redis_tools/async_io_redis.py
+++ sometools/async_tools/redis_tools/async_io_redis.py
@@ -15,7 +15,7 @@ class GeneralAsyncIoRedis:
     async def get_async_redis_conn(self, **kwargs):
         return await aioredis.create_redis_pool((kwargs.get('redis_host'), kwargs.get('redis_port')), encoding='utf-8',
                                                 db=kwargs.get('redis_db'), password=kwargs.get('redis_pwd'),
-                                                loop=asyncio.get_event_loop())
+                                                loop=asyncio.get_running_loop())
 
     async def aio_redis_conn_close(self):
         if self.aio_redis_conn:
